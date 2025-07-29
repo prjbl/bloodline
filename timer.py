6c9f5e02057e07e0d5caa0782ffc7a8f320eb3f9
Prjbl <257981950+prjbl@users.noreply.github.com>
2025-07-29 15:39:50 +0200

    Reworked timer; Added shift as helper key to keybind options; Adjusted counter; Fixed problem with input history after using the same history input twice in a row

--- timer.py
+++ timer.py
@@ -1,44 +1,70 @@
-import time as t
+from time import time
 
 class Timer:
     
-    __start_time: float = 0.0
-    __end_time: float = 0.0
-    __cache: float = 0
-    __timer_active: bool = False
-    __timer_toggled: bool = False
+    def __init__(self):
+        self._start_time: float = 0.0
+        self._end_time: float = 0.0
+        self._paused_time: float = 0.0
+        self._timer_active = False
+        self._timer_paused = False
+        self._observer: any = None
+    
+    
+    def set_observer(self, observer: any) -> None:
+        self._observer = observer
+    
+    
+    def _notify_observer(self, text: str, text_type: str) -> None:
+        self._observer(text, text_type)
     
     
     def start(self) -> None:
-        self.__timer_active = True
-        self.__start_time = t.time()
+        if not self._timer_active:
+            self._start_time = time()
+            self._timer_active = True
+            self._notify_observer("Timer has started", None)
     
     
     def toggle_pause(self) -> None:
-        if self.__timer_active:
-            self.__timer_toggled = not self.__timer_toggled
+        if self._timer_active:
+            self._timer_paused = not self._timer_paused
             
-            if self.__timer_toggled:
-                print("paused")
-                self.end() # problem with timer active = false
-                self.__cache = self.get_end_time()
+            if self._timer_paused:
+                self._pause()
             else:
-                print("resumed")
-                self.reset()
-                self.start()
-                self.__start_time -= self.__cache
+                self._resume()
+        else:
+            self._notify_observer("Timer has not started yet", "error")
+    
+    
+    def _pause(self) -> None:
+        elapsed_time_before_pause: float = time() - self._start_time
+        self._paused_time += elapsed_time_before_pause
+        self._notify_observer("Timer has been paused", None)
+    
+    
+    def _resume(self) -> None:
+        self._start_time, self._end_time = 0, 0
+        self._start_time = time()
+        self._start_time -= self._paused_time
+        self._notify_observer("Timer has been resumed", None)
     
     
     def end(self) -> None:
-        if self.__timer_active:
-            self.__end_time = t.time()
-        
-        self.__timer_active = False
+        if self._timer_active:
+            self._end_time = time()
+            self._timer_active = False
+            self._notify_observer(f"Timer has ended: {self.get_end_time()}", None)
     
     
-    def get_end_time(self) -> float:
-        return self.__end_time - self.__start_time
+    def get_end_time(self) -> int:
+        return int(self._end_time - self._start_time)
     
     
     def reset(self) -> None:
-        self.__start_time, self.__end_time = 0, 0
+        if not self._timer_active and self._start_time != 0:
+            self._start_time, self._end_time, self._paused_time = 0, 0, 0
+            self._notify_observer("Timer resetted", None)
+        elif self._timer_active:
+            self._notify_observer("Timer must be stopped for the reset to work", "error")
